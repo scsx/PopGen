755c3a5478ef8d0e19b39108210997084f387ba4
scsx <4182349+scsx@users.noreply.github.com>
2020-10-20 13:04:48 +0100

    ethnicities json

---
+++ js/ethnicities.js
@@ -0,0 +1,157 @@
+const types = [
+    {
+        "EU_North": {
+            "skins": [
+                { "default": "skin_white1" },
+                { "skin_white2": 34 }
+            ],
+            "eyes": [
+                { "default": "eyes_blue" },
+                { "eyes_green": 5 },
+                { "eyes_brown": 30 }
+            ],
+            "eyes_shape": [
+                { "default": "round" }
+            ],
+            "face_shape": [
+                { "default": "face_eu_north" }
+            ],
+            "hair": [
+                { "default": "hair_blonde" },
+                { "hair_black": 13 },
+                { "hair_brown": 33 },
+                { "hair_redhead": 5 }
+            ]
+        }
+    },
+    {
+        "EU_South": {
+            "skins": [
+                { "default": "skin_white2" },
+                { "skin_white1": 33 },
+                { "skin_white3": 34 },
+            ],
+            "eyes": [
+                { "default": "eyes_brown" },
+                { "eyes_green": 1 },
+                { "eyes_blue": 8 }
+            ],
+            "eyes_shape": [
+                { "default": "round" }
+            ],
+            "face_shape": [
+                { "default": "face_eu_south" }
+            ],
+            "hair": [
+                { "default": "hair_black" },
+                { "hair_brown": 20 },
+                { "hair_blonde": 8 },
+                { "hair_redhead": 1 }
+            ]
+        }
+    },
+    {
+        "Black": {
+            "skins": [
+                { "default": "skin_black2" },
+                { "skin_black1": 33 },
+                { "skin_black3": 34 },
+            ],
+            "eyes": [
+                { "default": "eyes_brown" }
+            ],
+            "eyes_shape": [
+                { "default": "round" }
+            ],
+            "face_shape": [
+                { "default": "face_black" }
+            ],
+            "hair": [
+                { "default": "hair_black" }
+            ]
+        }
+    },
+    {
+        "Indian": {
+            "skins": [
+                { "default": "skin_indian1" },
+                { "skin_indian2": 34 }
+            ],
+            "eyes": [
+                { "default": "eyes_brown" }
+            ],
+            "eyes_shape": [
+                { "default": "round" }
+            ],
+            "face_shape": [
+                { "default": "face_indian" }
+            ],
+            "hair": [
+                { "default": "hair_black" }
+            ]
+        }
+    },
+    {
+        "Asian": {
+            "skins": [
+                { "default": "skin_asian1" },
+                { "skin_asian2": 34 }
+            ],
+            "eyes": [
+                { "default": "eyes_brown" }
+            ],
+            "eyes_shape": [
+                { "default": "asian" }
+            ],
+            "face_shape": [
+                { "default": "face_asian" }
+            ],
+            "hair": [
+                { "default": "hair_black" }
+            ]
+        }
+    },
+    {
+        "Latin": {
+            "skins": [
+                { "default": "skin_white3" },
+                { "skin_white2": 15 },
+                { "skin_white4": 34 }
+            ],
+            "eyes": [
+                { "default": "eyes_brown" },
+                { "eyes_blue": 2 }
+            ],
+            "eyes_shape": [
+                { "default": "round" }
+            ],
+            "face_shape": [
+                { "default": "face_latin" }
+            ],
+            "hair": [
+                { "default": "hair_black" },
+                { "hair_brown": 8 },
+                { "hair_blonde": 2 }
+            ]
+        }
+    },
+    {
+        "Native": {
+            "skins": [
+                { "default": "skin_native1" }
+            ],
+            "eyes": [
+                { "default": "eyes_brown" }
+            ],
+            "eyes_shape": [
+                { "default": "eyes_native" }
+            ],
+            "face_shape": [
+                { "default": "face_native" }
+            ],
+            "hair": [
+                { "default": "hair_black" }
+            ]
+        }
+    }
+];
---
+++ js/locals.js
@@ -0,0 +1,22 @@
+const local_US = {
+    "races": [
+        { "EU_North": 50 },
+        { "EU_South": 13.3 },
+        { "Latin": 17.6 },
+        { "Black": 12.7 },
+        { "Asian": 4.2 },
+        { "Native": 1 },
+        { "Indian": 1.2 }
+    ]
+};
+
+const local_US1950 = {
+    "races": [
+        { "EU_North": 70 },
+        { "EU_South": 15 },
+        { "Latin": 3 },
+        { "Black": 10 },
+        { "Asian": 1 },
+        { "Native": 1 }
+    ]
+};
